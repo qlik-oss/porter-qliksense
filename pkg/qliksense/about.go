fad49b0d903f0350b4a51ec53c8e32eb965293ed
Andriy Bulynko <andriy.bulynko@gmail.com>
2020-01-14 13:59:51 -0500

    Extracting images from k8s yaml based on output from running kustomize (#13)

--- pkg/qliksense/about.go
+++ pkg/qliksense/about.go
@@ -1,14 +1,16 @@
 package qliksense
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
-	"os"
-	_ "os/exec"
+	"log"
+	"os/exec"
 	"path/filepath"
+	"reflect"
+	"sort"
 
-	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
 )
 
@@ -26,6 +28,7 @@ type AboutStep struct {
 type AboutArguments struct {
 	Step    `yaml:",inline"`
 	Version string `yaml:"version"`
+	Profile string `yaml:"profile"`
 }
 
 type VersionOutput struct {
@@ -36,17 +39,13 @@ type VersionOutput struct {
 // About The public method invoked by `porter` when performing an `Install` step that has a `qliksense` mixin step
 func (m *Mixin) About() error {
 	var (
-		payload          []byte
-		version          string
-		err              error
-		action           AboutAction
-		versionOut       VersionOutput
-		file             *os.File
-		realVersion, out []byte
-		scanner          *bufio.Scanner
-		images           []string
+		payload                       []byte
+		version                       string
+		err                           error
+		action                        AboutAction
+		versionOut                    VersionOutput
+		realVersion, out, kuzManifest []byte
 	)
-
 	if payload, err = m.getPayloadData(); err != nil {
 		return err
 	}
@@ -54,28 +53,20 @@ func (m *Mixin) About() error {
 		return err
 	}
 	if len(action.Steps) != 1 {
-		return errors.Errorf("expected a single step, but got %d", len(action.Steps))
+		return fmt.Errorf("expected a single step, but got %d", len(action.Steps))
 	}
-
 	if version = action.Steps[0].AboutArguments.Version; version == "bundled" {
-		realVersion, err = ioutil.ReadFile(filepath.Join(chartCache, "VERSION"))
-
-		if file, err = os.Open(filepath.Join(chartCache, "images-"+string(realVersion)+".txt")); err != nil {
+		if realVersion, err = ioutil.ReadFile(filepath.Join(chartCache, "VERSION")); err != nil {
+			log.Printf("error reading the VERSION file, error: %v\n", err)
 			return err
 		}
-		defer file.Close()
-
-		scanner = bufio.NewScanner(file)
-		images = make([]string, 0)
-		for scanner.Scan() {
-			images = append(images, scanner.Text())
-		}
-		if err = scanner.Err(); err != nil {
+		if kuzManifest, err = getKustomizeBuildOutput(action.Steps[0].AboutArguments.Profile); err != nil {
+			log.Printf("error executing kustomize, error: %v\n", err)
 			return err
 		}
 		versionOut = VersionOutput{
 			QliksenseVersion: string(realVersion),
-			Images:           images,
+			Images:           getImageList(kuzManifest),
 		}
 		if out, err = yaml.Marshal(versionOut); err != nil {
 			return err
@@ -85,4 +76,66 @@ func (m *Mixin) About() error {
 		// TODO: Means we are fetching from Git
 	}
 	return nil
+}
+
+func getKustomizeBuildOutput(path string) ([]byte, error) {
+	cmd := exec.Command("kustomize", "build", path)
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("executing kustomize process failed with error: %v, stderr: %v\n", err, string(stderr.Bytes()))
+		return nil, err
+	}
+	return stdout.Bytes(), nil
+}
+
+func getImageList(yamlContent []byte) []string {
+	decoder := yaml.NewDecoder(bytes.NewReader(yamlContent))
+	var resource map[string]interface{}
+	imageMap := make(map[string]bool)
+	for {
+		err := decoder.Decode(&resource)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Printf("error decoding yaml: %v\n", err)
+			}
+			break
+		}
+		traverseYamlDecodedMapRecursively(reflect.ValueOf(resource), []string{}, func(path []string, val interface{}) {
+			if len(path) >= 2 && path[len(path)-1] == "image" &&
+				(path[len(path)-2] == "containers" || path[len(path)-2] == "initContainers") {
+				if image, ok := val.(string); ok {
+					imageMap[image] = true
+				}
+			}
+		})
+	}
+	var sortedImageList []string
+	for image, _ := range imageMap {
+		sortedImageList = append(sortedImageList, image)
+	}
+	sort.Strings(sortedImageList)
+	return sortedImageList
+}
+
+func traverseYamlDecodedMapRecursively(val reflect.Value, path []string, visitorFunc func(path []string, val interface{})) {
+	kind := val.Kind()
+	switch kind {
+	case reflect.Interface:
+		traverseYamlDecodedMapRecursively(val.Elem(), path, visitorFunc)
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			traverseYamlDecodedMapRecursively(val.Index(i), path, visitorFunc)
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			traverseYamlDecodedMapRecursively(val.MapIndex(key), append(path, key.Interface().(string)), visitorFunc)
+		}
+	default:
+		if kind != reflect.Invalid {
+			visitorFunc(path, val.Interface())
+		}
+	}
 }
